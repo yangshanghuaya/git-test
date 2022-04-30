70e986a4775f583ea9d15f683142b89774a38f32
hua <hua@qq.com>
2022-04-30 09:45:49 +0800

    first commit

---
+++ src/main/java/com/pz/git/CitTest.java
@@ -0,0 +1,8 @@
+package com.pz.git;
+
+public class CitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!我是小龙女");
+        System.out.println("你好呀");
+    }
+}
