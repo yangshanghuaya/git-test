9c4d2219ba0e15dbdd0b5793f03307d7de608481
hua <hua@qq.com>
2022-04-30 10:01:31 +0800

    3 commit

--- src/main/java/com/pz/git/CitTest.java
+++ src/main/java/com/pz/git/CitTest.java
@@ -5,5 +5,6 @@ public class CitTest {
         System.out.println("hello git!我是小龙女");
         System.out.println("你好呀");
         System.out.println("版本二");
+        System.out.println("hello,紧急修护");
     }
 }
