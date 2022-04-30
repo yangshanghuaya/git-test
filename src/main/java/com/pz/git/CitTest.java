d3e73351f4e2395f38293ec17309c51a2444c21f
hua <hua@qq.com>
2022-04-30 09:48:43 +0800

    second commit

--- src/main/java/com/pz/git/CitTest.java
+++ src/main/java/com/pz/git/CitTest.java
@@ -4,5 +4,6 @@ public class CitTest {
     public static void main(String[] args) {
         System.out.println("hello git!我是小龙女");
         System.out.println("你好呀");
+        System.out.println("版本二");
     }
 }
